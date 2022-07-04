024a12aaf3589454baeadd712e084bfd23b7705e
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2022-07-04 04:02:29 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import "./App.css";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups"
+import { BrowserRouter, Routes } from 'react-router-dom';
+
+export const App = () => {
+    const activeMenu = true
+    return (
+        <div>
+            <BrowserRouter>
+                <div className="flex relative dark:bg-main-dark-bg">
+                    <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+                        <TooltipComponent content="Settings" position="Top">
+                            <button type="button" className="text-3xl p-3 
+                        hover:drop-shadow-xl
+                        hover:bg-light-gray 
+                        text-white" style={{ background: "blue", borderRadius: "50%" }}>
+                                <FiSettings />
+                            </button>
+                        </TooltipComponent>
+
+                    </div>
+                    {activeMenu ? (
+                        <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+                            Sidebar
+                        </div>
+                    ) : (
+                        <div className="w-0 ark:bg-secondary-dark-bg bg-white">
+                            Sidebar w-0
+                        </div>)}
+                    <div className={` dark:bg-main-bg bg-main-bg min-h-screen md:ml-72 w-full ${activeMenu ? " md:ml-72 " : " flex-2"}`}>
+                        <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg-navbar w-full">
+                            Navbar
+                        </div>
+                    </div>
+                    <div>
+                        <Routes>
+
+                        </Routes>
+                    </div>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
+};
